a575b818209e2e27bbd6f087b818ad4c72d9760c
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2024-06-29 00:42:28 +0900

    add vs test.vcxproj (WIP, build breaking)

--- libs/build.bat
+++ libs/build.bat
@@ -18,9 +18,7 @@ pushd %~dp0
 if not exist gflags_vsbuild mkdir gflags_vsbuild
 cmake -S gflags -B gflags_vsbuild -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DBUILD_TESTING=OFF --install-prefix=%cd%\gflags_vsbuild -G "Visual Studio 17 2022" || exit /b 1
 cmake --build gflags_vsbuild --config Debug || exit /b 1
-cmake --install gflags_vsbuild --config Debug || exit /b 1
 cmake --build gflags_vsbuild --config Release || exit /b 1
-cmake --install gflags_vsbuild --config Release || exit /b 1
 popd
 
 echo Building glog
@@ -28,9 +26,15 @@ pushd %~dp0
 if not exist glog_vsbuild mkdir glog_vsbuild
 cmake -S glog -B glog_vsbuild -DWITH_GFLAGS=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DHAVE_LIB_GFLAGS=1 --install-prefix=%cd%\glog_vsbuild -G "Visual Studio 17 2022" || exit /b 1
 cmake --build glog_vsbuild --config Debug || exit /b 1
-cmake --install glog_vsbuild --config Debug || exit /b 1
 cmake --build glog_vsbuild --config Release || exit /b 1
-cmake --install glog_vsbuild --config Release || exit /b 1
+popd
+
+echo Building gtest
+pushd %~dp0
+if not exist gtest_vsbuild mkdir gtest_vsbuild
+cmake -S gtest -B gtest_vsbuild -DBUILD_GMOCK=OFF -G "Visual Studio 17 2022" || exit /b 1
+cmake --build gtest_vsbuild --config Debug || exit /b 1
+cmake --build gtest_vsbuild --config Release || exit /b 1
 popd
 
 endlocal
--- src/tests/dummy_tests.cpp
+++ src/tests/dummy_tests.cpp
@@ -1,1 +1,6 @@
 #include <gtest/gtest.h>
+
+TEST(TestExample, SomeTest) {
+  EXPECT_TRUE(1 == 1);
+  EXPECT_EQ(3, 3);
+}
