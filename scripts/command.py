c1dc76b0c64a6fd584acefa21daeaa8b048191cd
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2024-06-30 15:30:48 +0900

    add --no-send-translate to command.py

--- scripts/command.py
+++ scripts/command.py
@@ -6,6 +6,7 @@ import icfp
 
 def command():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--no-send-translate', action='store_false', dest='send_translate', help='Do not translate ICFP to human readable text.')
     parser.add_argument('--no-translate', action='store_false', dest='translate', help='Do not translate ICFP to human readable text.')
     parser.add_argument('--no-communicate', action='store_false', dest='communicate')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print verbose output.')
@@ -17,7 +18,7 @@ def command():
         lines.append(line)
     command = '\n'.join(lines).strip()
     if args.communicate:
-        response = icfp.communicate(command, verbose=args.verbose , recv_translate=args.translate)
+        response = icfp.communicate(command, verbose=args.verbose, send_translate=args.send_translate, recv_translate=args.translate)
     else:
         response = icfp.icfp2ascii(command, verbose=args.verbose)
     print(response)
