114140d3625499b78a804ce3eaa4d7d4910f0b1f
Hitoshi Yoshida <github@peria.jp>
2024-06-30 10:42:27 +0900

    update commands to work for efficiency

--- scripts/icfp.py
+++ scripts/icfp.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python3
 
+import os
+import requests
+
 import icfp_peria
 import icfp_tsuzuki
 
-def communicate(command, verbose=False, translate=True):
-    return icfp_peria.communicate(command, verbose, translate)
-    # return icfp_tsuzuki.communicate(command, verbose)
+# https://boundvariable.space/communicate
+URL_DOMAIN = "boundvariable.space"
+TOKEN = os.environ["SANMA_TOKEN"]
+HEADERS = {"Authorization": f"Bearer {TOKEN}"}
+
+
+def communicate(ascii_command, verbose=False, translate=True):
+    icfp_command = 'S' + encrypt(ascii_command)
+    response = requests.post(f"https://{URL_DOMAIN}/communicate",
+                            data=icfp_command,
+                            headers=HEADERS)
+    response = response.text
+    if verbose:
+        print("ICFP response: ", response, file=sys.stderr)
+    if translate:
+        response = icfp2ascii(response, verbose)
+    return response
+
+
+def icfp2ascii(icfp, verbose=False):
+    return icfp_peria.icfp2ascii(icfp, verbose)
+
+
+mapping = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
+crypt_map = {}
+
+def decrypt(s):
+    return ''.join(mapping[ord(c) - 33] for c in s)
+
+def encrypt(s):
+    global crypt_map
+    if len(crypt_map) == 0:
+        for i in range(len(mapping)):
+            crypt_map[mapping[i]] = i
+
+    return ''.join(chr(crypt_map[c] + 33) for c in s)
+
 
 if __name__ == '__main__':
     print("icfp.py is not intended to be run as a script. Please run command.py instead.")
