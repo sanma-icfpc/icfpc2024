eb84d595f4591674c0d66d1769e6b549d1956de3
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2024-07-01 10:58:02 +0900

    lambdaman9 manual solution: (R51L51D)51

--- scripts/optimize_lambdaman6.py
+++ scripts/optimize_lambdaman6.py
@@ -1,16 +1,44 @@
-def length(recursive, multiply, initial):
-    program = 'B$ Lf ' + 'B$ vf ' * recursive + 'S' + 'L' * initial + ' Lx ' + 'B. ' * (multiply - 1) + 'vx ' * multiply
-    return len(program), initial * multiply ** recursive
+import sys
+import icfp
 
-best = None
-for r in range(1, 10):
-    for m in range(2, 20):
-        for i in range(1, 50):
-            program_size, answer_length = length(r, m, i)
-            if answer_length >= 199:
-                if best is None or best[0] > program_size:
-                    print(f'update best (program size {program_size}), r={r}, m={m}, i={i}')
-                    best = program_size, r, m, i
+def generate_shortest_for_repeat(char, min_repeat, fixed_initial=None):
+    enc_char = icfp.encrypt(char)
 
-program_size, r, m, i = best
-print(f'best (program size {program_size}), r={r}, m={m}, i={i}')
+    def length(recursive, multiply, initial):
+        program = 'B$ Lf ' + 'B$ vf ' * recursive + 'S' + enc_char * initial + ' Lx ' + 'B. ' * (multiply - 1) + 'vx ' * multiply
+        return len(program), initial * multiply ** recursive, program
+
+    best = None
+    for r in range(1, 10):
+        for m in range(2, 20):
+            if fixed_initial:
+                ilist = [fixed_initial]
+            else:
+                ilist = range(1, 50)
+            for i in ilist:
+                program_size, answer_length, program = length(r, m, i)
+                if answer_length >= min_repeat:
+                    if best is None or best[0] > program_size:
+                        best = program_size, r, m, i, program
+
+    program_size, r, m, i, program = best
+    print(f'best (program size {program_size}), r={r}, m={m}, i={i}')
+    return program, program_size, r, m, i
+
+if int(sys.argv[1]) == 6:
+    program, program_size, r, m, i = generate_shortest_for_repeat('R', 199)
+    print(program)
+
+if int(sys.argv[1]) == 9:
+    print(generate_shortest_for_repeat('L', 50, fixed_initial=1))
+
+    program = f'''
+repeat6_to_50 := Ly B$ Lf B$ vf B$ vf vy Lx B. B. vx vx vx 
+repeat_50 := Ly B$ Lf B$ vf B$ vf B$ vf vy Lx B. B. B. vx vx vx vx
+cell_gen := Lr B. B. B$ vr S{icfp.encrypt('R')} B$ vr S{icfp.encrypt('L')} S{icfp.encrypt('D')}
+cell := B$ $cell_gen $repeat_50
+all := B$ ( La B$ va B$ $cell_gen va ) $repeat_50
+'''
+    main = icfp.reduce_extended_icfp(program, 'all')
+    print(len(main))
+    print(main)
